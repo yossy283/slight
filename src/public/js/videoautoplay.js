5a458c12095461836a33d64c0ff15097fe0c1cdf
boderline <yossy283@gmail.com>
2023-02-02 00:34:35 +0900

    mv change

--- src/public/js/videoautoplay.js
+++ src/public/js/videoautoplay.js
@@ -3,9 +3,3 @@ var detectVideo = document.getElementById('video');
 
 //detectVideo の再生エラーを検出する
 var video_promise = detectVideo.play();
-video_promise.catch(function(error){
-  isLowPowerMode = true;
-
-  //演出目的の大きい動画のautoplay属性を外し、不要なリクエストを発生させない
-  //detectVideo.removeAttribute("autoplay"); 
-});
